b34d3fd6f67e1e43f9aebfc3b8e1e2a88d21222f
gleb-erokhin <gleb.erokhin@gmail.com>
2021-09-21 19:56:04 +0300

    4 variant of hw3 course osnovy python

--- home work 1.py
+++ home work 1.py
@@ -3,17 +3,34 @@
 #print(number + 2)
 
 # HW2
-number = int(input('Введите число от 0 до 10 (не включительно): '))
-while 10 < number > 0:
-    print('Введено не верное число')
-    number = int(input('Введите число от 0 до 10 (не включительно): '))
-else:
-    print(number ** 2)
+#number = int(input('Введите число от 0 до 10 (не включительно): '))
+#while 10 < number > 0:
+#    print('Введено не верное число')
+#    number = int(input('Введите число от 0 до 10 (не включительно): '))
+#else:
+#    print(number ** 2)
+
+# HW3
+
+name = 'Вася'
+sur_name = 'пупкин'
+age = int(input('Введите Возраст: '))
+
+if age <= 30:
+    veight = int(input('Введите Вес: '))
+    if 50 <= veight < 120:
+        print(name, age, 'год, ', 'вес', veight, '- Вы в хорошем состоянии')
+elif age > 30:
+    veight = int(input('Введите Вес: '))
+    if 50 > veight > 120:
+        print(name, age, 'год, ', 'вес', veight, '- Вам следует заняться собой')
+elif age > 40:
+    veight = int(input('Введите Вес: '))
+    if 50 <= veight < 120:
+        print(name, age, 'год, ', 'вес', veight, '- Вам требуется врачебный осмотр')
 
 
 
 
 
-    #break
-    #if number > 10:
-        #print('число не верно, введите снова')
+
