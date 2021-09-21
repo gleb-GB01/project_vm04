842e126b3692e311befe8d2b34b3971945de9531
gleb-erokhin <gleb.erokhin@gmail.com>
2021-09-21 22:23:44 +0300

    All Home work 1
    3 home work of course python
    21.09.21

--- home work 1.py
+++ home work 1.py
@@ -1,14 +1,14 @@
 # HW1
-#number = int(input('Введите число: '))
-#print(number + 2)
+number = int(input('Введите число: '))
+print(number + 2)
 
 # HW2
-#number = int(input('Введите число от 0 до 10 (не включительно): '))
-#while 10 < number > 0:
-#    print('Введено не верное число')
-#    number = int(input('Введите число от 0 до 10 (не включительно): '))
-#else:
-#    print(number ** 2)
+number = int(input('Введите число от 0 до 10: '))
+while 10 < number > 0:
+    print('Введено не верное число')
+    number = int(input('Введите число от 0 до 10: '))
+else:
+    print(number ** 2)
 
 # HW3
 #Пациент в хорошем состоянии, если ему до 30 лет и вес от 50 и до 120 кг,
